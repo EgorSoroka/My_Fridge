f29b9c6a376f3c02084cc4cfe982be3cea049ef1
George Magpie <prich845@gmail.com>
2020-11-08 18:17:29 +0300

    Добавлено сохранение состояния при повороте смартфона

--- app/src/main/java/com/george/my_fridge/MainActivity.kt
+++ app/src/main/java/com/george/my_fridge/MainActivity.kt
@@ -1,30 +1,57 @@
 package com.george.my_fridge
 
-import androidx.appcompat.app.AppCompatActivity
+/**
+ *  @author George Magpie
+ *
+ */
+
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
     var counter: Int = 0
+    val str = "Ты че жрать-то собрался??!!"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val createButton = findViewById<Button>(R.id.create_button)
         val eatButton = findViewById<Button>(R.id.eat_button)
 
         val eatCounter = findViewById<TextView>(R.id.counter)
 
-        createButton.setOnClickListener(){
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt("counter")
+            if (counter < 0) {
+                eatCounter.text = str
+            } else eatCounter.text = "${counter}"
+
+        }
+
+        createButton.setOnClickListener() {
+
             eatCounter.text = "${++counter}"
         }
 
-        eatButton.setOnClickListener(){
+        eatButton.setOnClickListener() {
+
             eatCounter.text = "${--counter}"
-            if (counter < 0) eatCounter.text = "Ты че жрать-то собрался??!!"
+            if (counter < 0) eatCounter.text = str
         }
 
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putInt("counter", counter)
 
     }
+
+
 }
